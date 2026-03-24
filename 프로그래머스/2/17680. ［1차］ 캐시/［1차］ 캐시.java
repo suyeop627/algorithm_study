7ec27bc1d0ff835138e026e95a4555b69fe49530
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2026-03-24 12:55:54 +0900

    [level 2] Title: [1차] 캐시, Time: 38.67 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,47 +1,23 @@
+import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         int answer = 0;
-        if(cacheSize==0) return cities.length * 5;
+        List<String> caches = new LinkedList<>();
+        if(cacheSize ==0) return cities.length*5;
         
-        String[] caches = new String[cacheSize];
-        
-        int cachedCount = 0;
-        for(String city: cities){
+        for(String city : cities){
             city = city.toLowerCase();
-            int index = indexOf(caches, city);
-            if(index == -1){
-                answer+=5;
-                
-                if(cachedCount < cacheSize){
-                    for(int i = cachedCount; i > 0; i-- ){
-                        caches[i] = caches[i-1];
-                    }      
-                    caches[0] = city;
-                    cachedCount++;
-                }else{
-                    for(int i = cacheSize-1; i > 0; i-- ){
-                        caches[i] = caches[i-1];
-                    }      
-                    caches[0] = city;
-                }
-            } else{
-                for(int i = index; i > 0; i-- ){
-                    caches[i] = caches[i-1];
-                }      
-                caches[0] = city;
-                answer++;
-            }
             
+            if(caches.remove(city)){
+                answer++;
+            }else{
+                answer+=5;
+                if(caches.size() == cacheSize){
+                    caches.remove(cacheSize -1);
+                }
+            }
+            caches.add(0,city);
         }
         return answer;
-    }
-    
-    private int indexOf(String[] caches, String city){
-        for(int i = 0; i < caches.length; i++){
-            if(caches[i] != null && caches[i].equals(city)){
-                return i;
-            }
-        }
-        return -1;
     }
 }
