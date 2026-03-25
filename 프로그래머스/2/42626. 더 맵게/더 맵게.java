a5227e1e06aee5abe840812c64d5f0be31e34195
suyeop627 <112303446+suyeop627@users.noreply.github.com>
2026-03-25 21:01:58 +0900

    [level 2] Title: 더 맵게, Time: 1459.04 ms, Memory: 124 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int s : scoville){
+            pq.offer(s);
+        }
+        
+        while(pq.peek() < K){
+            if(pq.size() < 2){
+                return -1;
+            }
+            pq.offer(pq.poll() + pq.poll()*2);
+            answer++;
+        }
+        
+        return answer;
+    }
+}
